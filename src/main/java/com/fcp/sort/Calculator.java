d4a50e588092186a0e62c39c8e9c0d6d5d18ef32
frankcreen <frankcreen1019@gmail.com>
2019-06-07 16:23:47 +0800

    Jacoco测试配置

---
+++ src/main/java/com/fcp/sort/Calculator.java
@@ -0,0 +1,37 @@
+package com.fcp.sort;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return Math.addExact(a, b);
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multi(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public int switchCase(int a) {
+        switch (a) {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            default:
+                return 4;
+        }
+    }
+}
+
---
+++ src/test/java/com/fcp/sort/CalculatorTest.java
@@ -0,0 +1,130 @@
+package com.fcp.sort;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        // given
+        int a = 10;
+        int b = 20;
+
+        // when
+        final int addNumber = calculator.add(a, b);
+
+        // then
+        assertEquals(30, addNumber);
+    }
+
+    @Test
+    public void testSub() {
+        // given
+        int a = 20;
+        int b = 2;
+
+        // when
+        final int subNumber = calculator.sub(a, b);
+
+        // then
+        assertEquals(18, subNumber);
+    }
+
+    @Test
+    public void testMulti() {
+        // given
+        int a = 3;
+        int b = 2;
+
+        // when
+        final int multiResult = calculator.multi(a, b);
+
+        // then
+        assertEquals(6, multiResult);
+    }
+
+    @Test
+    public void testDiv() {
+        // given
+        int a = 10;
+        int b = 2;
+
+        // when
+        final int divResult = calculator.div(a, b);
+
+        // then
+        assertEquals(5, divResult);
+    }
+
+    @Test
+    public void testSwitchCase_1() {
+        // given
+        int a = 1;
+
+        // when
+        final int result = calculator.switchCase(a);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSwitchCase_2() {
+        // given
+        int a = 2;
+
+        // when
+        final int result = calculator.switchCase(a);
+
+        // then
+        assertEquals(2, result);
+    }
+
+//    @Disabled("取消")
+//    @Test
+//    public void testSwitchCase_3() {
+//        // given
+//        int a = 3;
+//
+//        // when
+//        final int result = calculator.switchCase(a);
+//
+//        // then
+//        assertEquals(3, result);
+//    }
+
+//    @Disabled("取消")
+//    @Test
+//    public void testSwitchCase_4() {
+//        // given
+//        int a = 4;
+//
+//        // when
+//        final int result = calculator.switchCase(a);
+//
+//        // then
+//        assertEquals(4, result);
+//    }
+
+
+
+   @Test
+   public void testSqrt() {
+        //given
+       double a = 9D;
+
+       //when
+       final double result = calculator.sqrt(a);
+
+       //then
+       assertEquals(3D,result);
+
+   }
+
+
+}
